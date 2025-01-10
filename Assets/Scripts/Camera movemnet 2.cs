fa4764f57cdedb5fbf62e25f53dca7d85ff558ab
Abi <abig4ilbrown@icloud.com>
2025-01-10 13:22:55 +0000

    mini game implement

--- Assets/Scripts/Camera movemnet 2.cs
+++ Assets/Scripts/Camera movemnet 2.cs
@@ -2,43 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCam : MonoBehaviour
+public class CamMove2 : MonoBehaviour
 {
-    public float sensX = 100f;
-    public float sensY = 100f;
-
-    public Transform orientation;
-
-    float xRotation = 0f;
-    float yRotation = 0f;
-
+    // this is =me creating a constant forward camerera movement 
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        
     }
 
     private void Update()
     {
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-
-
-        yRotation += mouseX;
-        orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
-
-        xRotation -= mouseY;
-        if (xRotation < -10)
-        {
-            xRotation = -10;
-        }
-        else if (xRotation > 50)
-        {
-            xRotation = 50;
-        }
-
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        
     }
 }
