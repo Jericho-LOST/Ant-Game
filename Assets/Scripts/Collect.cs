b239ea3881e660bc642a5eb0e6b5f46199126182
Abi <abig4ilbrown@icloud.com>
2025-01-24 15:51:54 +0000

    pain :)

--- Assets/Scripts/Collect.cs
+++ Assets/Scripts/Collect.cs
@@ -8,18 +8,18 @@ public class Collect : MonoBehaviour
     int coins = 0;
 
     [SerializeField] Text coinsText;
-    //[SerializeField] AudioSource coinSound;
+    [SerializeField] AudioSource coinSound;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Coin"))
         {
-           // coinSound.Play();
+           coinSound.Play();
             Destroy(other.gameObject);
             coins++;
            coinsText.text = "Fruits:" + coins; 
 
-           // coinSound.Play();
+           
 
 
         }
